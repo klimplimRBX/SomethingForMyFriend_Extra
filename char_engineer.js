bb492eb4656a2277883a3dcb7f9d956b43dcf19b
klimplimRBX <klimplimnimiriumtuto@gmail.com>
2026-07-25 14:59:54 -0300

    Update char_engineer.js

--- char_engineer.js
+++ char_engineer.js
@@ -28,8 +28,8 @@ const SENTRY_TIERS = [
   /* 0 - Sentry           */ { name:'Sentry',           hp:100, dmg:10, cd:1.0, projSpd:800,  predictive:false },
   /* 1 - Upgraded Sentry  */ { name:'Upgraded Sentry',  hp:150, dmg:15, cd:0.9, projSpd:850,  predictive:false },
   /* 2 - Rifle Sentry     */ { name:'Rifle Sentry',     hp:200, dmg:25, cd:1.5, projSpd:1200, predictive:true  },
-  /* 3 - Minigun Sentry   */ { name:'Minigun Sentry',   hp:250, dmg:2,  cd:0.1, projSpd:1300, predictive:true  },
-  /* 4 - War Machine      */ { name:'War Machine Sentry',hp:300, dmg:5, cd:0.1, projSpd:1400, predictive:true  },
+  /* 3 - Minigun Sentry   */ { name:'Minigun Sentry',   hp:250, dmg:8,  cd:0.4, projSpd:1300, predictive:true  },
+  /* 4 - War Machine      */ { name:'War Machine Sentry',hp:300, dmg:8, cd:0.2, projSpd:1400, predictive:true  },
 ];
 const SENTRY_SZ = 50;
 
