8fd48eb82a12e9c05204b02bbf69edffbaee6172
klimplimRBX <klimplimnimiriumtuto@gmail.com>
2026-07-26 15:23:39 -0300

    Update char_darkninja.js

--- char_darkninja.js
+++ char_darkninja.js
@@ -21,7 +21,7 @@ const DKN_KATANA_STAGES = [
   { dmg: 125, cd: 0.6   },
   { dmg: 100,  cd: 0.4   },
   { dmg: 75,  cd: 0.2   },
-  { dmg: 100,  cd: 0.05 },
+  { dmg: 100,  cd: 0.01 },
 ];
 const DKN_KATANA_RANGE       = ENGI_SLEDGE_RANGE + 15; // pendência: ajustar testando in-game
 
